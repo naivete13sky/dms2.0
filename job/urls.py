770448585e4c419e19915ceb9abbb8dd93240075
naivete13sky <chen320821@163.com>
2022-11-29 19:04:03 +0800

    20221129 19

--- job/urls.py
+++ job/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
 
     path('JobListView',login_required(views.JobListView.as_view()),name='JobListView'),#类视图，用来组普通用户展示料号列表的。
 
-
     path('tag/<str:tag_slug>/',  views.JobListView.as_view(), name='job_list_by_tag'), # 这里的参数类型不要写slug，否则又会忽视中文，写str就行了
 
     path('JobDetailView/<int:pk>', views.JobDetailView.as_view(), name='JobDetailView'),#类视图。
