586e42b3c6b21fbc5fa732e860faf29c26cddf79
naivete13sky <chen320821@163.com>
2022-11-29 15:01:06 +0800

    20221129 6

--- job/views.py
+++ job/views.py
@@ -43,6 +43,7 @@ class JobListView(ListView):
         context['job_field_verbose_name'] = [Job._meta.get_field('id').verbose_name,
                                              Job._meta.get_field('job_name').verbose_name,
                                              Job._meta.get_field('file_compressed').verbose_name,
+                                             Job._meta.get_field('from_object_pcb_factory').verbose_name,
                                              Job._meta.get_field('status').verbose_name,
                                              Job._meta.get_field('updated').verbose_name,
                                              Job._meta.get_field('author').verbose_name,
@@ -101,7 +102,17 @@ class JobListView(ListView):
                 current_query_data.save()
             context['jobs'] = context['jobs'].filter(author__username__contains = query_job_author)
 
-
+            # 料号来源-板厂筛选
+            query_job_from_object_pcb_factory = self.request.GET.get("query_job_from_object_pcb_factory", False)
+            context['query_job_from_object_pcb_factory'] = query_job_from_object_pcb_factory
+            # 先把本次筛选条件存储起来
+            current_query_data = QueryData.objects.get(author=self.request.user)
+            if query_job_from_object_pcb_factory != None:
+                current_query_data.query_job_from_object_pcb_factory = query_job_from_object_pcb_factory
+                current_query_data.save()
+            if context['query_job_from_object_pcb_factory'] != "":
+                context['jobs'] = context['jobs'].filter(
+                    from_object_pcb_factory__name_simple__contains=context['query_job_from_object_pcb_factory'])
 
 
 
