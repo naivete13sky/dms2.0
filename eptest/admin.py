8bc2c4266b0ea453e43d93c3f306927a5cf11121
naivete13sky <chen320821@163.com>
2022-11-30 19:23:43 +0800

    20221130 12 JobInfoForTest admin list_display tag_list

--- eptest/admin.py
+++ eptest/admin.py
@@ -4,14 +4,22 @@ from mptt.admin import MPTTModelAdmin
 
 @admin.register(JobForTest)
 class JobForTestAdmin(admin.ModelAdmin):
-    list_display = ('job','file','test_usage_for_epcam_module','standard_odb','vs_result_ep','vs_result_g','bug_info','status','author','publish','create_time','tags','remark')
-
-    search_fields = ('job','author__username','vs_result_ep','vs_result_g','tags',)
+    list_display = ('job','file','test_usage_for_epcam_module','standard_odb','vs_result_ep','vs_result_g','bug_info','status','author','publish','create_time','tag_list','remark')
+    list_filter = ('tags',)
+    search_fields = ('job','author__username','vs_result_ep','vs_result_g',)
     prepopulated_fields = {'remark': ('job',)}
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     # ordering = ('recipe_status', 'receive_date',)
     list_per_page = 10
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return ",".join(o.name for o in obj.tags.all())
+
+
 
 
 class EpcamModuleAdmin(MPTTModelAdmin):
