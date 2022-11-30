0c3b90a500a86335edae95129ea7fff0abe15b11
naivete13sky <chen320821@163.com>
2022-11-30 16:38:19 +0800

    20221130 6

--- eptest/admin.py
+++ eptest/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import JobForTest,EpcamModule
-
+from mptt.admin import MPTTModelAdmin
 
 @admin.register(JobForTest)
 class JobForTestAdmin(admin.ModelAdmin):
@@ -13,13 +13,18 @@ class JobForTestAdmin(admin.ModelAdmin):
     # ordering = ('recipe_status', 'receive_date',)
     list_per_page = 10
 
-@admin.register(EpcamModule)
-class EpcamModuleAdmin(admin.ModelAdmin):
-    list_display = ('name','lft','rght','tree_id','level','parent_id',)
+# @admin.register(EpcamModule)
+# class EpcamModuleAdmin(admin.ModelAdmin):
+#     list_display = ('name','lft','rght','tree_id','level','parent_id',)
+#     search_fields = ('name',)
+#     list_per_page = 10
 
-    search_fields = ('name','lft','rght','tree_id','level','parent_id')
-    # prepopulated_fields = {'remark': ('job',)}
-    # raw_id_fields = ('author',)
-    # date_hierarchy = 'publish'
-    # ordering = ('recipe_status', 'receive_date',)
+
+# admin.site.register(EpcamModule,MPTTModelAdmin)
+
+
+class EpcamModuleAdmin(MPTTModelAdmin):
+    list_display = ('name','lft','rght','tree_id','level','parent_id',)
+    search_fields = ('name',)
     list_per_page = 10
+admin.site.register(EpcamModule, EpcamModuleAdmin)
