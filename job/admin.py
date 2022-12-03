f8ebeb4fd52179d78a3b433be14d8bb810abbfa2
naivete13sky <chen320821@163.com>
2022-12-03 14:14:47 +0800

    20221203 1

--- job/admin.py
+++ job/admin.py
@@ -6,7 +6,8 @@ from .models import Job,JobInfoForDevTest
 class JobAdmin(admin.ModelAdmin):
     list_display = ('job_name','file_compressed','has_file_type','status','author','from_object_pcb_factory','from_object_pcb_design','publish','create_time','tags')
 
-    search_fields = ('job_name','author__username','from_object_pcb_factory__name_simple')
+    search_fields = ('job_name',)
+    list_filter = ('has_file_type', 'status', 'author','from_object_pcb_factory','from_object_pcb_design','tags')
     prepopulated_fields = {'remark': ('job_name',)}
     raw_id_fields = ('author','from_object_pcb_factory','from_object_pcb_design')
     date_hierarchy = 'publish'
@@ -19,6 +20,7 @@ class JobInfoForDevTestAdmin(admin.ModelAdmin):
     list_display = ('job','status','author','file_odb','has_step_multi','job_type_1','job_type_2','job_type_3','publish','updated','remark')
 
     search_fields = ('job','status','author','has_step_multi',)
+    list_filter = ('job', 'status', 'author',  'job_type_1', 'job_type_2', 'job_type_3')
     prepopulated_fields = {'remark': ('job',)}
     raw_id_fields = ('author','job')
     date_hierarchy = 'publish'
