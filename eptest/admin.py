0320322e858b270fd68b861a0cfa80ae4a34fa4c
naivete13sky <chen320821@163.com>
2022-12-06 15:54:05 +0800

    20221206 8

--- eptest/admin.py
+++ eptest/admin.py
@@ -22,7 +22,7 @@ admin.site.site_title = '料号管理系统'
 
 @admin.register(JobForTest)
 class JobForTestAdmin(admin.ModelAdmin):
-    list_display = ('id','job_parent_link','job_name','get_layer_info_link','get_test_file_link','file_type','test_usage_for_epcam_module','get_standard_odb_link','vs_result_ep','get_vs_info_g_link','bug_info','status','author','updated','tag_list','remark')
+    list_display = ('id','job_parent_link','job_name','get_layer_info_link','file','file_type','test_usage_for_epcam_module','get_standard_odb_link','vs_result_ep','get_vs_info_g_link','bug_info','status','author','updated','tag_list','remark')
     list_filter = ('tags','file_type','author','test_usage_for_epcam_module',)
     search_fields = ('id','job_parent__job_name','job_name','author__username','vs_result_ep','vs_result_g',)
     prepopulated_fields = {'remark': ('job_name',)}
@@ -46,7 +46,7 @@ class JobForTestAdmin(admin.ModelAdmin):
 
     def get_layer_info_link(self, obj):
         if obj.bool_layer_info == 'true':
-            return mark_safe(f'<a href="../../../../eptest//{obj.id}/">查看</a>')
+            return mark_safe(f'<a href="../../../../eptest/layer/?q=one_job_layer/{obj.id}/">查看</a>')
         else:
             return mark_safe(f'<a href="../../../../eptest/get_layer_name_from_org/{obj.id}/">生成</a>')
     get_layer_info_link.short_description = '层别'
@@ -71,12 +71,12 @@ class JobForTestAdmin(admin.ModelAdmin):
 
     def get_test_file_link(self, obj):
         if obj.file:
-            return mark_safe(f'<a href="../../../../media/{obj.file}/" title=" {obj.file} ">下载</a>')
+            return mark_safe(f'<a href="../../../../media/{obj.file}" title=" {obj.file} ">下载</a>')
     get_test_file_link.short_description = '测试料号'
 
     def get_standard_odb_link(self, obj):
         if obj.standard_odb:
-            return mark_safe(f'<a href="../../../../media/{obj.standard_odb}/" title=" {obj.standard_odb} ">下载</a>')
+            return mark_safe(f'<a href="../../../../media/{obj.standard_odb}" title=" {obj.standard_odb} ">下载</a>')
     get_standard_odb_link.short_description = '标准料号'
 
 
@@ -109,6 +109,31 @@ class LayerAdmin(admin.ModelAdmin):
     def job_link(self, obj):
         return mark_safe(f'<a href="../../eptest/jobfortest/{obj.job_id}/change/">打开</a>')
     job_link.short_description = '所属料号'
+
+    # 默认的查询集合
+    def get_queryset(self, request):
+        return super(LayerAdmin, self).get_queryset(request).all().order_by("-id")
+
+    # 根据关键字进行查询集合,自定义查询。如果搜索内容包括了“one_job_layer/123”,则说明是要查某个料号（ID：123）的层信息
+    def get_search_results(self, request, queryset, search_term):
+        print("search_term:", search_term)
+        queryset, use_distinct = super(LayerAdmin, self).get_search_results(request, queryset, search_term)
+        print("queryset:",queryset)
+        print("use_distinct:", use_distinct)
+        if "one_job_layer/" in search_term:
+            print("搜索指定料号下的layer信息")
+            queryset = self.model.objects.filter(job=int(search_term.split("/")[1]))
+            return queryset,use_distinct
+        # try:
+        #     search_term_as_int = int(search_term)
+        #     print("search_term_as_int:",search_term_as_int)
+        #     queryset &= (self.model.objects.filter(gift_rule_id=search_term_as_int) |
+        #                  self.model.objects.filter(user_id=search_term_as_int) |
+        #                  self.model.objects.filter(activity_id=search_term))
+        # except:
+        #     pass
+        return queryset, use_distinct
+
 
 
 
