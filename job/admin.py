6584cd6addc082e412b0e4bec337591bf0fac5c1
naivete13sky <chen320821@163.com>
2022-12-16 09:54:00 +0800

    20221216 2

--- job/admin.py
+++ job/admin.py
@@ -6,11 +6,11 @@ from .models import Job,JobInfoForDevTest
 class JobAdmin(admin.ModelAdmin):
     list_display = ('id','job_name','file_compressed','has_file_type','status','author','from_object_pcb_factory','from_object_pcb_design','publish','create_time','tag_list')
 
-    search_fields = ('id','job_name',)
+    search_fields = ('=id','job_name',)
     list_filter = ('has_file_type', 'status', 'author__username','from_object_pcb_factory','from_object_pcb_design','tags')
     prepopulated_fields = {'remark': ('job_name',)}
     raw_id_fields = ('author','from_object_pcb_factory','from_object_pcb_design')
-    date_hierarchy = 'publish'
+    # date_hierarchy = 'publish'
     # ordering = ('recipe_status', 'receive_date',)
     list_per_page = 10
 
@@ -32,19 +32,22 @@ class JobAdmin(admin.ModelAdmin):
 
 @admin.register(JobInfoForDevTest)
 class JobInfoForDevTestAdmin(admin.ModelAdmin):
-    list_display = ('id','job','status','author','file','has_step_multi','job_type_1','job_type_2','job_type_3','publish','updated','remark')
+    list_display = ('id','job','status','author','file','has_step_multi','job_type_1','job_type_2','job_type_3','updated','tag_list','remark')
 
-    search_fields = ('id','job__job_name','status','author__username','has_step_multi',)
-    list_filter = ( 'status', 'author',  'job_type_1', 'job_type_2', 'job_type_3')
+    search_fields = ('=id','job__job_name','status','author__username','has_step_multi',)
+    list_filter = ( 'status', 'author',  'job_type_1', 'job_type_2', 'job_type_3','tags',)
     prepopulated_fields = {'remark': ('job',)}
     raw_id_fields = ('author','job')
-    date_hierarchy = 'publish'
+    # date_hierarchy = 'publish'
     # ordering = ('recipe_status', 'receive_date',)
     list_per_page = 10
     exclude = ('author', 'publish')
 
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
 
-
+    def tag_list(self, obj):
+        return ",".join(o.name for o in obj.tags.all())
 
 
     '''保存时自动设置author为当前登录用户'''
