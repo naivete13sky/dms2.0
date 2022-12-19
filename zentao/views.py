8c1f9081225af79c6cc89a85c097381068b34a08
naivete13sky <chen320821@163.com>
2022-12-19 16:12:02 +0800

    20221219 6

--- zentao/views.py
+++ zentao/views.py
@@ -1,8 +1,12 @@
+import datetime
+from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views.generic.base import View, TemplateView
+from sqlalchemy import create_engine
+import pandas as pd
 
 
 class BugView_0(View):
@@ -18,4 +22,42 @@ class BugView(TemplateView):
         kwargs = super().get_context_data(**kwargs)
         kwargs['name'] = 'BugView'
         kwargs['title'] = "BugView"
+
+        engine = create_engine("mysql+mysqlconnector://chencheng:hWx9pWk5d5J@10.97.80.36:3336/zentao")
+        sql = '''SELECT a.*,b.name productname,c.realname createbywho,d.realname assignedtowho from zt_bug a
+                LEFT JOIN zt_product b on a.product=b.id
+                LEFT JOIN zt_user c on a.openedBy=c.account
+                LEFT JOIN zt_user d on a.assignedTo=d.account
+                where a.deleted='0'
+                '''
+        bug_pd = pd.read_sql_query(sql, engine)
+        # print(bug_pd)
+        # bug_pd.to_excel(r'C:\Users\Administrator\Desktop\pd.xlsx')
+        bug_pd['create_date'] = pd.to_datetime(bug_pd['openedDate']).dt.date
+        bug_static_by_create_date = bug_pd.groupby('create_date')["id"].count()
+
+        dict_create_date_number = {'create_date': bug_static_by_create_date.index, 'numbers': bug_static_by_create_date.values}
+        bug_static_by_create_date_pd=pd.DataFrame(dict_create_date_number)
+        # print(bug_static_by_create_date_pd)
+        # bug_static_by_create_date_pd.to_excel(r'C:\Users\Administrator\Desktop\pd2.xlsx')
+        # print(bug_static_by_create_date_pd.columns.tolist())#查看有哪些字段
+        # print(datetime.date(datetime.now()))#今天
+        today_new_bug_count = bug_static_by_create_date_pd[(bug_static_by_create_date_pd.create_date == datetime.date(datetime.now()))]['numbers'].values[0]
+        # print(today_new_bug_count)
+        kwargs['today_new_bug_count'] = today_new_bug_count
+
+
+
+
+
+        sql = '''SELECT * from zt_module
+        '''
+        bug_moudle_pd = pd.read_sql_query(sql, engine)
+        # print(bug_moudle_pd)
+
+        sql = '''SELECT * from zt_module a
+        where a.grade=1 
+        '''
+
+
         return kwargs
