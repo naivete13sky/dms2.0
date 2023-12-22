8ffc96290a3147b56a9fc2dbeb0c064a7770b907
naivete13sky <chen320821@163.com>
2023-12-22 15:52:27 +0800

    eptest for vue tags ok

--- eptest/serializers.py
+++ eptest/serializers.py
@@ -10,6 +10,7 @@ class JobForTestSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())  # 指定author字段的显示方式
     test_usage_for_epcam_module = serializers.StringRelatedField(source='test_usage_for_epcam_module.name',
                                                                  read_only=True)
+    tags = serializers.StringRelatedField(many=True)
     class Meta:
         model = JobForTest
         fields = '__all__'
