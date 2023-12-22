b9c600e6bf5c690b6d16e85950902a351e53ef2a
naivete13sky <chen320821@163.com>
2023-12-22 15:15:33 +0800

    eptest for vue 13 ok

--- eptest/serializers.py
+++ eptest/serializers.py
@@ -5,6 +5,7 @@ from .models import JobForTest
 from django.contrib.auth.models import User  # 导入User模型
 
 class JobForTestSerializer(serializers.ModelSerializer):
+    status = serializers.CharField(source='get_status_display')
     # author = serializers.StringRelatedField(source='author.username')  # 指定author字段的显示方式
     author = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())  # 指定author字段的显示方式
     test_usage_for_epcam_module = serializers.StringRelatedField(source='test_usage_for_epcam_module.name',
